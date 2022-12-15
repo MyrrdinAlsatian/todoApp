f6a547f1ea719c5f1068fd6cf4537785216a3140
Myrrdin <stephan.jeanba@gmail.com>
2022-12-15 15:29:36 +0100

    [🚧 WIP](Back): add Task CRUD back \&\& form

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/task')]
+class TaskController extends AbstractController
+{
+    #[Route('/', name: 'app_task_index', methods: ['GET'])]
+    public function index(TaskRepository $taskRepository): Response
+    {
+        return $this->render('task/index.html.twig', [
+            'tasks' => $taskRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_task_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, TaskRepository $taskRepository): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+        $form->remove('isDone');
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $taskRepository->save($task, true);
+            $this->addFlash('success', sprintf("La tâche %s a bien été ajouté", $task->getTitle()));
+            return $this->redirectToRoute('app_task_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('task/new.html.twig', [
+            'task' => $task,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_task_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Task $task, TaskRepository $taskRepository): Response
+    {
+        $form = $this->createForm(TaskType::class, $task);
+        $form->remove("createdAt");
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $taskRepository->save($task, true);
+            $this->addFlash('success', sprintf("La tâche %s a bien été modifié", $task->getTitle()));
+            return $this->redirectToRoute('app_task_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('task/edit.html.twig', [
+            'task' => $task,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/toggle', name: 'app_task_toggle', methods: ['POST'])]
+    public function toggle(Request $request, Task $task, TaskRepository $taskRepository): Response
+    {
+        $task->toggle(!$task->isDone());
+        $taskRepository->save($task,true);
+        if($task->isDone())
+        {
+            $this->addFlash('success', sprintf("La tâche %s a bien été marquée comme complété", $task->getTitle()));
+            return $this->redirectToRoute('app_task_index');
+        }
+        $this->addFlash('success', sprintf("La tâche %s a bien été marquée comme non complété", $task->getTitle()));
+
+        return $this->redirectToRoute('app_task_index');
+        
+    }
+
+    #[Route('/{id}', name: 'app_task_delete', methods: ['POST'])]
+    public function delete(Request $request, Task $task, TaskRepository $taskRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$task->getId(), $request->request->get('_token'))) {
+            $taskRepository->remove($task, true);
+        }
+
+        return $this->redirectToRoute('app_task_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
