bbc56de17ed1f599dbd580e7f7b1b2b400959273
Myrrdin <stephan.jeanba@gmail.com>
2022-12-15 10:13:46 +0100

    [🚧 WIP](Back): add Form constraint  and type for register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,27 +20,41 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class, ['label' => "Nom d'utilisateur"])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => "Conditions d'utilisation" ,
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => "Veuillez accepter nos conditions d'utilisation",
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('email', EmailType::class, [
+                'label'=>"Adresse mail",
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Veuillez entrer votre adresse mail"
+                    ])
+                    ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'type'=>PasswordType::class,
+                'invalid_message'=>" les deux mots de passe doivent correspondre",
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmez votre mot de passe'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit faire {{ limit }} caracters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
